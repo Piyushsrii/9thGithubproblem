f4482fab360f5de5076c663ca0e0a9b74c380a74
piyushsrii <piyushnamo888@gmail.com>
2020-07-13 21:38:12 +0530

    commited in uc5

--- AllEmployeeCase.sh
+++ AllEmployeeCase.sh
@@ -1,1 +1,23 @@
 Welcome All Cases
+
+#UserCase5
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+EmployeeWorkdaysPerMonths=20
+EmployeeRatePerHrs=20
+emplCheck=${RANDOM:0:1}
+randomVariable=$emplCheck
+case $randomVariable in
+         $isPartTime)
+                           Hrs=8;
+                            ;;
+         $isFullTime)
+                           Hrs=4;
+                            ;;
+                   *)
+                           Hrs=0;
+                            ;;
+                         esac
+salary=$(($Hrs*$EmployeeRatePerHrs*$EmployeeWorkdaysPerMonths))
+echo $salary
