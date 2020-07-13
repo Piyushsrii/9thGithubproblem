b2c6bcc9c2805e07877aad3f18fc6cce1ba405a2
piyushsrii <piyushnamo888@gmail.com>
2020-07-13 21:29:05 +0530

    commited in uc1

--- AllEmployeeCase.sh
+++ AllEmployeeCase.sh
@@ -1,1 +1,12 @@
 Welcome All Cases
+#User Case-1
+
+#!/bin/bash -x
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [[ $isPresent -eq $randomCheck ]]
+then
+echo "Employee is present"
+else
+echo "Employee is not present"
+fi
