df27b18ebe2313bd58b17448f83f582c8417e4ef
piyushsrii <piyushnamo888@gmail.com>
2020-07-13 21:43:33 +0530

    commited in uc7

--- AllEmployeeCase.sh
+++ AllEmployeeCase.sh
@@ -1,1 +1,22 @@
 Welcome All Cases
+#User Case7
+
+#!/bin/bash -x
+function WorkHours(){
+isPartTime=1
+isFullTime=2
+EmpCheck=${RANDOM:0:1}
+randomVariable=$EmpCheck
+            case $randomVariable in
+                 $isFullTime)
+                                Hrs=8;
+                                 ;;
+                 $isPartTime)
+                                Hrs=4;
+                                 ;;
+                           *)
+                                Hrs=0
+                                 ;;
+                          esac
+}
+WorkHours
